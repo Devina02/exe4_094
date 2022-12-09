7735d74e877ada804fba0789c3e1b3b1058397a5
Devina02 <devina.fitriapngs@gmail.com>
2022-12-09 16:06:04 +0700

    menambahkan if else pada boolean pop

--- Program.cs
+++ Program.cs
@@ -17,17 +17,40 @@ namespace exe4_094
             top = -1;
             max = size;
         }
-        public void push(int item)
+        Boolean push(int item)
         {
+            int val;
             if (top == max - 1)
             {
-                Console.WriteLine("Stack Overflow");
-                return;
+
+                Console.WriteLine("\n Overflow");
+                return false;
             }
             else
             {
-                elmnt[++top] = item;
+                Console.WriteLine("Enter the value?");
+                val = Convert.ToInt32(Console.ReadLine());
+                top = top + 1;
+                elmnt[top] = val;
+                return true;
             }
         }
+        Boolean pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine(" Underflow ");
+                return false;
+            }
+
+            else
+
+            {
+                top = top - 1;
+                Console.WriteLine(" Item popped ");
+                return true;
+            }
+        }
+
     }
 }
